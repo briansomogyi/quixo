149680aefa5abda5166aecec54fb242582f4fefc
etimarita <etian.marita@emanuel.ro>
2024-04-30 09:45:50 +0300

    COMM

--- elemente.js
+++ elemente.js
@@ -1,16 +1,21 @@
-function setup() {
-    createCanvas(650, 650, WEBGL);
-    camera(0, 0, 50*sqrt(3), 0, 0, 0, 0, 1, 0);
-    perspective(PI/3, 1, 5*sqrt(3), 500*sqrt(3));
-    describe('a white box rotating in 3D space');
-  }
-  
-  function draw() {
-    background(200);
-    //rotateX(frameCount * 0.01);
-    //rotateY(frameCount * 0.01);
-    box(50);
-  }
+let cube = 50; // Size of each square on the board
 
-  
-  
+function setup() {
+  createCanvas(5 * cube, 5 * cube);
+  drawBoard();
+}
+
+function drawBoard() {
+  let lightColor = color(255, 206, 158); 
+  let x, y;
+
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      x = i * cube;
+      y = j * cube;
+      
+      fill(lightColor);
+      rect(x, y, cube, cube);
+    }
+  }
+}
