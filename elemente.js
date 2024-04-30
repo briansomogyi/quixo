467d5bead0505f7890fb523035b1526d9f07b346
etimarita <etian.marita@emanuel.ro>
2024-04-30 09:40:21 +0300

    cube

--- elemente.js
+++ elemente.js
@@ -1,5 +1,5 @@
 function setup() {
-    createCanvas(100, 100, WEBGL);
+    createCanvas(650, 650, WEBGL);
     camera(0, 0, 50*sqrt(3), 0, 0, 0, 0, 1, 0);
     perspective(PI/3, 1, 5*sqrt(3), 500*sqrt(3));
     describe('a white box rotating in 3D space');
