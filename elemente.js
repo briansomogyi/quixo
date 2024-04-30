83a7a0cce7e93605de943f0a2d5e8ef9d1c2a972
briansomogyi <brian.somogyi@emanuel.ro>
2024-04-30 09:49:29 +0300

    Am adus la olaltă informația din mai multe surse.

--- elemente.js
+++ elemente.js
@@ -1,5 +1,8 @@
 function setup() {
   createCanvas(800, 600, WEBGL);
+  // camera(0, 0, 50*sqrt(3), 0, 0, 0, 0, 1, 0);
+  //   perspective(PI/3, 1, 5*sqrt(3), 500*sqrt(3));
+  //   describe('a white box rotating in 3D space');
 }
 
 function draw() {
@@ -7,11 +10,11 @@ function draw() {
 
   // Create a cube at position (0, 0, 0) with side length 100
   let side = 100;
-  translate(-100, -100, side); // Move to the center of the cube
+  translate(0, 0, side); // Move to the center of the cube
   rotateX(frameCount * 0.01); // Rotate around X-axis
   rotateY(frameCount * 0.02); // Rotate around Y-axis
   box(side); // Draw the cube
-  translate(0, 0, side); // Move to the center of the cube
-  box(side); // Draw the cube
+  // translate(0, 0, side); // Move to the center of the cube
+  // box(side); // Draw the cube
   
 }
