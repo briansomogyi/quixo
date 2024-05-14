068ff7f5f26954b06d7cc9c2844edb24cfe91020
giulianaa23 <giulianacristea06@gmail.com>
2024-05-14 18:27:47 +0300

    final

--- sketch.js
+++ sketch.js
@@ -1,34 +1,54 @@
 let input, button, greeting;
 
 function setup() {
-  // create canvas
-  createCanvas(710, 400);
+    // create canvas
+    createCanvas(710, 400);
 
-  input = createInput();
-  input.position(20, 65);
+    input = createInput();
+    input.position(20, 65);
 
-  button = createButton('submit');
-  button.position(input.x + input.width, 65);
-  button.mousePressed(greet);
+    button = createButton('submit');
+    button.position(input.x + input.width, 65);
+    button.mousePressed(greet);
 
-  greeting = createElement('h2', 'what is your name?');
-  greeting.position(20, 5);
+    greeting = createElement('h2', 'what is your name?');
+    greeting.position(20, 5);
 
-  textAlign(CENTER);
-  textSize(50);
+    textAlign(CENTER);
+    textSize(50);
 }
 
 function greet() {
-  const name = input.value();
-  greeting.html('hello ' + name + '!');
-  input.value('');
+    const name = input.value();
+    greeting.html('hello ' + name + '!');
+    input.value('');
 
-  for (let i = 0; i < 200; i++) {
-    push();
-    fill(random(255), 255, 255);
-    translate(random(width), random(height));
-    rotate(random(2 * PI));
-    text(name, 0, 0);
-    pop();
-  }
+    let color;
+    if (name == "Etian") {
+        color = "yellow";
+
+    } else {
+        if (name == "Brian") {
+            color = "green";
+
+        } else {
+            if (name == "Giulia" || name == "Giuliana") {
+                color = "pink";
+
+            } else {
+                color = "white";
+
+            }
+        }
+    }
+
+    for (let i = 0; i < 200; i++) {
+        push();
+        fill(color);
+        translate(random(width), random(height));
+        rotate(random(2 * PI));
+        text(name, 0, 0);
+        pop();
+    }
 }
+const names = ["Etian", "Brian", "Giulia",];
